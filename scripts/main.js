964020f9cf9e69ce011632be1d13992c1b2eb95c
LaFave1 <160167777+LaFave1@users.noreply.github.com>
2024-06-12 03:31:13 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+let test1 = 'test'; // string
+console.log(test1, typeof test1);
+
+let test2 = 123; // number
+console.log(test2, typeof test2); 
+
+let test3 = 10.3; // number
+console.log(test3, typeof test3);
+
+let test4 = test2 > test3; // boolean (bool) true/false
+console.log(test4, typeof test4);
+
+let test5 = null; // Object
+console.log(test5, typeof test5);
+
+let test6 = NaN; // number
+console.log(test6, typeof test6);
+
+let test7 = Infinity;
+console.log(test7, typeof test7);
+
+console.log(0.1 + 0.2);
+
+console.log(
+    (0.1 * 10) + (0.2 * 10) / 10
+);
+
+console.log(
+    Number.MAX_VALUE   
+);
+console.log(2 + 2);
+let a = 1 + '1';
+console.log(a);
+// Двійкова система (бінарна) - основа 2, використовуються тільки цифри 0 і 1. Наприклад, 2 в двійковій системі це 10, а 4 — це 100.
+//Вісімкова система(октальна) - основа 8, використовуються цифри від 0 до 7. Наприклад, 4 у вісімковій системі це 4.
+//Шістнадцяткова система(хексадецимальна) - основа 16, використовуються цифри від 0 до 9 і літери від A до F(де A = 10, B = 11 і так далі).Наприклад, 4 у шістнадцятковій системі це 4.
+
